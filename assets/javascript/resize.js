1824cf06fbfc06a323c99e1a0b16f3acae0c062d
leannethng <leanne.thng@gmail.com>
2015-05-26 14:25:32 -0700

    added in resize.js
    Add this to get full image working for BG in both height and width.
    
    Still need to style text, want to look at text in css shapes

---
+++ assets/javascript/resize.js
@@ -0,0 +1,18 @@
+
+//this triggers the resize when the window is pulled
+jQuery(window).resize(function(){
+  resize();
+});
+
+
+//the $ means on load
+$(function() {
+	resize();
+});
+
+
+//this is the funtion that is being used
+function resize() {
+	var _outerHeight = jQuery(window).height();
+	jQuery('#top-section').height(_outerHeight);
+}
